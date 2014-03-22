0faca685065ba8365aa7a68650201c86c72e82f4
Omar Rizwan <omar.rizwan@gmail.com>
2014-03-22 05:17:39 -0400

    multiple def and level support, start of term tracking.

--- src/ten_hundred/core.cljs
+++ src/ten_hundred/core.cljs
@@ -7,39 +7,77 @@
 (enable-console-print!)
 
 (defn colorize-token [token]
-  (if (re-matches #"\w+" token)
+  (if (re-matches #"[\w']+" token)
     (if (dict/words (string/lower-case token))
       (dom/span nil token)
       (dom/span #js {:className "uncommon"} token))
     token))
 
 (defn colorize [text]
-  (let [tokens (re-seq #"[^\w]+|\w+" text)]
-    (println tokens)
-    (map colorize-token tokens)))
+  (->> text
+       (re-seq #"[^\w']+|[\w']+")
+       (map colorize-token)))
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state
+  (atom [[{:term ""
+           :meaning ""}]]))
 
-(defn handle-change [e owner state]
-  (om/set-state! owner :definition (.. e -target -value)))
+(defn handle-term-change [e definition]
+  (om/update! definition :term (.. e -target -value)))
 
-(defn definition-view [app owner]
+(defn handle-meaning-change [e definition]
+  (om/update! definition :meaning (.. e -target -value)))
+
+(defn definition-view [definition owner]
   (reify
-    om/IInitState
-    (init-state [_]
-      {:definition "Enter text"})
-    om/IRenderState
-    (render-state [this state]
+    om/IRender
+    (render [this]
       (dom/div nil
-        (dom/input #js {:type "text" :value "Term"})
+        (dom/input #js {:type "text" :placeholder "Term"
+                        :value (:term definition)
+                        :onChange #(handle-term-change % definition)})
         (dom/div #js {:className "definition"}
           (apply dom/pre #js {:className "bg"}
-                 (colorize (:definition state)))
+                 (colorize (:meaning definition)))
           (dom/textarea #js {:className "edit"
-                             :value (:definition state)
-                             :onChange #(handle-change % owner state)}))))))
+                             :value (:meaning definition)
+                             :onChange #(handle-meaning-change % definition)}))))))
+
+(defn add-definition [level]
+  (om/transact! level #(conj % {:term ""
+                                :meaning ""})))
+
+(defn level-view [level owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+        (apply dom/div nil
+          (om/build-all definition-view level))
+        (dom/button #js {:onClick #(add-definition level)} "+def")))))
+
+(defn find-terms [levels]
+  (apply merge
+         (map-indexed (fn [idx level]
+                        (apply merge
+                               (map #(hash-map (:term %)
+                                               idx)
+                                    (remove #(empty? (:term %)) level))))
+                      levels)))
+
+(defn add-level [app]
+  (om/transact! app #(conj % [])))
+
+(defn app-view [app owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+        (apply dom/div nil
+          (om/build-all level-view app))
+        (dom/button #js {:onClick #(add-level app)} "+level")))))
 
 (om/root
-  definition-view
+  app-view
   app-state
   {:target (. js/document (getElementById "app"))})
