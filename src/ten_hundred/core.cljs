43ee61173658337a27b7c51315fd07cfc248b894
Omar Rizwan <omar.rizwan@gmail.com>
2014-04-17 22:59:55 -0700

    Start of drag. Kinda buggy. Focus is now a app-wide channel taking UUIDs instead of a channel for each definition

--- src/ten_hundred/core.cljs
+++ src/ten_hundred/core.cljs
@@ -12,23 +12,19 @@
 (enable-console-print!)
 
 ; graph view
-(defn handle-node-click [focus]
-  (put! focus :focus)
-  false)
-
-(defn render-node [g n value]
+(defn render-node [g n value focus]
   (let [node (.node g n)
         x (.-x value)
         y (.-y value)
         width (.-width value)
         height (.-height value)
 
-        label (.-label node)
-        focus (.-focus node)]
+        label (.-label node)]
     (dom/g #js {:transform (str "translate(" (- x (/ width 2))
                                 "," (- y (/ height 2)) ")")}
            (dom/rect #js {:className "nodeRect"
-                          :onClick #(handle-node-click focus)
+                          :onClick #(do (put! focus n)
+                                        false)
                           :width width
                           :height height})
            (dom/text #js {:className "nodeLabel"
@@ -42,7 +38,7 @@
                  :d (graph/points->svg-path
                      (graph/calc-points layout e))}))
 
-(defn render-graph [fullscreen-graph terms levels]
+(defn render-graph [fullscreen-graph terms levels focus]
   (let [g (->> levels
                (graph/adjacency-list terms)
                (graph/graph))
@@ -55,7 +51,7 @@
                #js {:width width
                     :height height}
                #js {:width 380
-                    :height 320
+                    :height 280
                     :viewBox (str "0 0 "
                                   width " "
                                   height)})
@@ -66,23 +62,24 @@
             <path d=\"M 0,0 V 4 L6,2 Z\" class=\"arrow\" />
             </marker>"}})
       (apply dom/g #js {:className "nodes"}
-        (map #(render-node g % (.node layout %))
+        (map #(render-node g % (.node layout %) focus)
              (.nodes layout)))
       (apply dom/g #js {:className "edges"}
         (map #(render-edge layout %)
              (.edges layout))))))
 
-(defn handle-term-click [term-state]
-  (put! (:focus term-state) :focus))
+(defn handle-term-click [focus term-state]
+  (put! focus (:uuid term-state)))
 
 (defn find-term [terms token]
   (last (filter #(= (:term %) token) terms)))
 
-(defn colorize-word [terms word]
+(defn colorize-word [terms focus word]
   (let [lc-word (string/lower-case word)]
     (cond (and terms (find-term terms lc-word))
           (dom/span #js {:className "defined"
                          :onClick #(handle-term-click
+                                    focus
                                     (find-term terms lc-word))}
                     word)
 
@@ -100,8 +97,7 @@
 (defn empty-definition []
   {:uuid (uuid/uuid-string (uuid/make-random-uuid))
    :term ""
-   :meaning ""
-   :focus (chan)})
+   :meaning ""})
 
 (defn make-example-state []
   {:id (uuid/uuid-string (uuid/make-random-uuid))
@@ -170,51 +166,94 @@
                     "x")))))
 
 ; definition view
-(def drag-state (atom {:dragged nil
-                       :placeholder nil}))
-(defn drag-start [e]
-  (swap! drag-state (assoc :dragged (.-currentTarget e)))
-  (let [data-transfer (.-dataTransfer e)]
+(def drag-state
+  (atom {:dragging nil
+         :dragging-el nil
+         :delete-original! nil
+         :drop! nil
+         :placeholder
+         (let [placeholder (.createElement js/document "div")]
+           (set! (.-className placeholder) "placeholder")
+           placeholder)}))
+
+(defn drag-start [e definition delete-original]
+  (let [current-target (.-currentTarget e)
+        data-transfer (.-dataTransfer e)]
+    (swap! drag-state #(assoc %
+                         :dragging definition
+                         :dragging-el current-target
+                         :delete-original! delete-original))
     (set! (.-effectAllowed data-transfer) "move")
-    (.setData data-transfer "text/html" (.-currentTarget e))))
+    (.setData data-transfer "text/html" current-target)))
 
 (defn drag-end [e]
-  (let [dragged (:dragged @drag-state)]
-    (set! (.-display (.-style dragged)) "block")
-    (.removeChild (.-parentNode dragged) placeholder)
+  (let [dragging (:dragging @drag-state)
+        dragging-el (:dragging-el @drag-state)
+        delete-original! (:delete-original! @drag-state)
+        drop! (:drop! @drag-state)
+        placeholder (:placeholder @drag-state)]
+    (.removeChild (.-parentNode placeholder) placeholder)
+    (set! (.-display (.-style dragging-el)) "")
+    (drop!)
+    (delete-original!)
+    (swap! drag-state #(dissoc % :dragging :dragging-el :delete-original! :drop!))))
 
-    ))
+(defn drag-over [e owner drop]
+  (.preventDefault e)
+  (let [target (om/get-node owner)
+        parent (.-parentNode target)
 
-(defn drag-over [e])
+        dragging (:dragging @drag-state)
+        placeholder (:placeholder @drag-state)
+
+        bounding-rect (.getBoundingClientRect target)
+        from-top (js/Math.abs (- (.-top bounding-rect) (.-pageY e)))
+        from-bottom (js/Math.abs (- (.-bottom bounding-rect) (.-pageY e)))
+
+        position (if (< from-top from-bottom)
+                   :above
+                   :below)]
+    (set! (.-display (.-style (:dragging-el @drag-state))) "none")
+
+    (case position
+      :above (.insertBefore parent placeholder target)
+      :below (.insertBefore parent placeholder (.-nextSibling target)))
+    
+    (swap! drag-state #(assoc % :drop! (fn [] (drop dragging position))))))
 
 (defn handle-term-change [e definition]
   (om/update! definition :term (.. e -target -value)))
 
-(defn handle-meaning-change [e definition]
-  (om/update! definition :meaning (.. e -target -value)))
+(defn handle-meaning-change [e owner definition]
+  (let [textarea (.-target e)
+        bg (om/get-node owner "meaningBg")]
+    (js/window.setTimeout 
+     (fn [_]
+       (let [height (str (.-scrollHeight textarea) "px")]
+         (set! (.-height (.-style textarea)) height)
+         (set! (.-height (.-style bg)) height)))
+     0)
+    (om/update! definition :meaning (.-value textarea))))
 
 (defn definition-view [definition owner]
   (reify
-    om/IInitState
-    (init-state [_]
-      {:show-expansion false})
-    om/IWillMount
-    (will-mount [_]
-      (let [focus (:focus definition)]
-        (go (loop []
-          (let [e (<! focus)]
-            (cond (= e :focus) (-> (om/get-node owner)
-                                   (.getElementsByClassName "edit")
-                                   (aget 0)
-                                   (.focus)))
-            (recur))))))
     om/IRenderState
-    (render-state [this {:keys [level-idx show-expansion terms inspect delete-definition]}]
+    (render-state [this {:keys [level-idx terms
+                                focus drop-on
+                                inspect delete-definition]}]
       (dom/div #js {:className "definition"
+                    :id (:uuid definition)
                     :draggable true
-                    :onDragStart drag-start
+                    :onDragStart
+                    (fn [e] (drag-start e @definition
+                                        #(put! delete-definition (:uuid @definition))))
                     :onDragEnd drag-end
-                    :onDragOver drag-over}
+                    :onDragOver
+                    #(drag-over % owner
+                                (fn [dragging position]
+                                  (put! drop-on [(:uuid @definition)
+                                                 dragging
+                                                 position])))}
         (dom/button #js {:className "expand"
                          :onClick #(put! inspect (:uuid @definition))}
                     "i")
@@ -223,14 +262,16 @@
                         :value (:term definition)
                         :onChange #(handle-term-change % definition)})
         (dom/button #js {:className "remove"
-                         :onClick #(put! delete-definition @definition)}
+                         :onClick #(put! delete-definition (:uuid @definition))}
                     "x")
         (dom/div #js {:className "meaning"}
           (dom/textarea #js {:className "edit"
+                             :ref "meaningEdit"
                              :value (:meaning definition)
-                             :onChange #(handle-meaning-change % definition)})
-          (apply dom/pre #js {:className "bg"}
-            (word-map #(colorize-word terms %)
+                             :onChange #(handle-meaning-change % owner definition)})
+          (apply dom/pre #js {:className "bg"
+                              :ref "meaningBg"}
+            (word-map #(colorize-word terms focus %)
                       (:meaning definition))))))))
 
 ; level view
@@ -242,25 +283,57 @@
   (reify
     om/IInitState
     (init-state [_]
-      {:delete-definition (chan)})
+      {:drop-on (chan)
+       :delete-definition (chan)})
     om/IWillMount
     (will-mount [_]
-      (let [delete-definition (om/get-state owner :delete-definition)]
+      (let [drop-on (om/get-state owner :drop-on)
+            delete-definition (om/get-state owner :delete-definition)
+            delete-level (om/get-state owner :delete-level)]
         (go (loop []
-          (let [definition (<! delete-definition)]
+          (let [[uuid dragging-definition position] (<! drop-on)]
             (om/transact! level
-              (fn [level] (vec (remove #(= definition %) level))))
+              (fn [level]
+                (let [splice-idx
+                      (first (keep-indexed
+                              (fn [idx definition]
+                                (if (= (:uuid definition) uuid)
+                                  idx
+                                  nil))
+                              level))]
+                  (js/console.log splice-idx position)
+                  (case position
+                    :above (apply conj
+                                  (subvec level 0 splice-idx)
+                                  dragging-definition
+                                  (subvec level splice-idx))
+                    :below (apply conj
+                                  (subvec level 0 (inc splice-idx))
+                                  dragging-definition
+                                  (subvec level (inc splice-idx)))))))
+            (recur))))
+
+        (go (loop []
+          (let [uuid (<! delete-definition)]
+            (om/transact! level
+              (fn [level]
+                (let [[n m] (split-with #(not= (:uuid %) uuid) level)]
+                  (vec (concat n (rest m))))))
             (recur))))))
     om/IRenderState
-    (render-state [this {:keys [inspect delete-level level-idx terms delete-definition]}]
+    (render-state [this {:keys [focus inspect delete-level level-idx terms
+                                drop-on delete-definition]}]
       (dom/div #js {:className "level"}
-        (apply css-trans-group #js {:transitionName "defTrans"}
-          (om/build-all definition-view level
-                        {:key :uuid
-                         :init-state {:inspect inspect
-                                      :delete-definition delete-definition}
-                         :state {:level-idx level-idx
-                                 :terms terms}}))
+        (when (seq level)
+          (apply css-trans-group #js {:transitionName "defTrans"}
+            (om/build-all definition-view level
+                          {:key :uuid
+                           :init-state {:focus focus
+                                        :inspect inspect
+                                        :drop-on drop-on
+                                        :delete-definition delete-definition}
+                           :state {:level-idx level-idx
+                                   :terms terms}})))
         (dom/button #js {:className "addDefinition"
                          :onClick #(add-definition level)} "+def")
         (dom/button #js {:className "deleteLevel"
@@ -274,21 +347,20 @@
        (map #(hash-map :uuid (:uuid %)
                        :term (string/lower-case (:term %))
                        :meaning (:meaning %)
-                       :level-idx idx
-                       :focus (:focus %))
+                       :level-idx idx)
             (remove #(empty? (:term %)) level)))
      levels)))
 
 (defn add-level [app]
   (om/transact! app :levels #(conj % [(empty-definition)])))
 
-(defn build-level [app delete-level inspect level-idx level]
+(defn build-level [app delete-level focus inspect level-idx level]
   (om/build level-view level
-            {:init-state {:inspect inspect
+            {:init-state {:focus focus
+                          :inspect inspect
                           :delete-level delete-level}
              :state {:level-idx level-idx
-                     :terms (find-terms
-                             (take level-idx (:levels app)))}}))
+                     :terms (find-terms (take level-idx (:levels app)))}}))
 
 (defn handle-fullscreen-graph [owner fullscreen-graph]
   (om/update-state! owner :fullscreen-graph not))
@@ -310,12 +382,24 @@
     (init-state [_]
       {:inspect-id nil
        :fullscreen-graph false
+       :focus (chan)
        :inspect (chan)
        :delete-level (chan)})
+
     om/IWillMount
     (will-mount [_]
-      (let [inspect (om/get-state owner :inspect)
+      (let [focus (om/get-state owner :focus)
+            inspect (om/get-state owner :inspect)
             delete-level (om/get-state owner :delete-level)]
+        (go (loop []
+          (let [id (<! focus)]
+            (-> js/document
+                (.getElementById id)
+                (.getElementsByClassName "edit")
+                (aget 0)
+                (.focus))
+            (recur))))
+
         (go (loop []
           (let [id (<! inspect)]
             (om/set-state! owner :inspect-id id)
@@ -326,15 +410,16 @@
             (om/transact! app :levels
               (fn [levels] (vec (remove #(= level %) levels))))
             (recur))))))
+
     om/IRenderState
     (render-state [this {:keys [inspect-id
-                                inspect
+                                focus inspect
                                 fullscreen-graph
                                 delete-level]}]
       (dom/div #js {:className "app"}
         (dom/div #js {:className "levelsWrapper"}
           (apply dom/div #js {:className "levels"}
-                 (conj (vec (map-indexed #(build-level app delete-level inspect
+                 (conj (vec (map-indexed #(build-level app delete-level focus inspect
                                                        %1 %2)
                                          (:levels app)))
                        (dom/button #js {:className "addLevel"
@@ -357,9 +442,9 @@
                                           " fullscreen"))
                         :onClick #(handle-fullscreen-graph owner fullscreen-graph)}
             (render-graph fullscreen-graph
-                          (find-terms
-                           (:levels app))
-                          (:levels app))
+                          (find-terms (:levels app))
+                          (:levels app)
+                          focus)
             (dom/div #js {:className "controls"}
               (dom/button #js {:onClick (fn [] 
                                           (docs/save! @app)
--- src/ten_hundred/docs.cljs
+++ src/ten_hundred/docs.cljs
@@ -2,29 +2,11 @@
   (:require [cljs.core.async :refer [chan]]
             [ajax.core :refer [GET PUT]]))
 
-(defn- strip-transient [app]
-  (update-in
-   app
-   [:levels]
-   (fn [levels]
-     (mapv (fn [level]
-            (mapv #(dissoc % :focus) level))
-          levels))))
-
 (defn save! [app]
   (PUT (str "https://ten-hundred-files.s3.amazonaws.com/" (:id app))
-       {:params (strip-transient app)}))
-
-(defn- readd-transient [app]
-  (update-in
-   app
-   [:levels]
-   (fn [levels]
-     (mapv (fn [level]
-            (mapv #(assoc % :focus (chan)) level))
-          levels))))
+       {:params app}))
 
 (defn load [id handler error-handler]
   (GET (str "https://ten-hundred-files.s3.amazonaws.com/" id)
-       {:handler (comp handler readd-transient)
+       {:handler handler
         :error-handler error-handler}))
